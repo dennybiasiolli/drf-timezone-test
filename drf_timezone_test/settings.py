dc543898fb9ace924f4c767f7a9386e04264c2fc
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2025-10-06 14:30:42 +0200

    allowing users to filter by value_dt

--- drf_timezone_test/settings.py
+++ drf_timezone_test/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'timezone_test_app',
+    'django_filters',
     'rest_framework',
 ]
 
--- timezone_test_app/views.py
+++ timezone_test_app/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.filters import OrderingFilter
 
@@ -8,6 +9,9 @@ from .serializers import TimezoneTestSerializer
 class TimezoneTestViewSet(viewsets.ModelViewSet):
     queryset = TimezoneTest.objects.all()
     serializer_class = TimezoneTestSerializer
-    filter_backends = [OrderingFilter]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = {
+        'value_dt': ['gte', 'lte', 'exact', 'gt', 'lt'],
+    }
     ordering = ['value_dt']
     # permission_classes = [permissions.IsAuthenticated]
